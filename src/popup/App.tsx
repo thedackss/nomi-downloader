68adefeb27d95ae02495b10190f96cc9cf343b71
Diego Zamora <diegozar02@gmail.com>
2026-02-13 05:23:44 +0000

    Refactor App component to initialize settings and fetch Nomis data on mount

--- src/popup/App.tsx
+++ src/popup/App.tsx
@@ -1,11 +1,35 @@
+import { useEffect } from "react";
 import "./app.styles.scss";
+import { NomiList } from "./components/NomiList";
+import { useSettings } from "./hooks/useSettings";
+import { useNomi } from "./hooks/useNomi";
 
 function App() {
+    const { InitializeSettings } = useSettings();
+    const { Nomis, checkSelectedNomi, fetchNomis } = useNomi();
+
+    useEffect(() => {
+        async function initialize() {
+            InitializeSettings();
+            await fetchNomis();
+        }
+        initialize();
+    }, []);
+
+    useEffect(() => {
+        if (Nomis.list.length > 0) {
+            checkSelectedNomi();
+        }
+    }, [Nomis.list, checkSelectedNomi]);
+
     return (
         // body
         <>
             <h1>Nomi Downloader</h1>
-            <div className="main-container"></div>
+            <div className="main-container">
+                <NomiList />
+                <NomiList />
+            </div>
         </>
     );
 }
