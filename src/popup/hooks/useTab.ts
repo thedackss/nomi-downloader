dd84f26a81d7cfc4beb49799c957460226afb7da
Diego Zamora <diegozar02@gmail.com>
2026-02-13 05:16:04 +0000

    Add useTab hook for retrieving and verifying the current active tab

---
+++ src/popup/hooks/useTab.ts
@@ -0,0 +1,36 @@
+import { useCallback } from "react";
+type Tab = chrome.tabs.Tab;
+
+export const useTab = () => {
+    const getCurrentTab = useCallback(async () => {
+        try {
+            const tabs: Tab[] = await chrome.tabs.query({
+                active: true,
+                currentWindow: true,
+            });
+
+            if (tabs.length < 1) throw new Error("No active tab found");
+
+            const tab: Tab = tabs[0];
+
+            const verified =
+                !!tab.url && !!tab.id && !!tab.url.includes("beta.nomi.ai");
+
+            if (!verified) throw new Error("Not a Nomi tab");
+
+            return {
+                tab,
+                tabId: tab.id ?? -1,
+                tabUrl: tab.url ?? "",
+                ok: true,
+            };
+        } catch (error) {
+            if (error instanceof Error) {
+                console.error(error.message);
+            }
+            return { tab: null, tabId: null, tabUrl: null, ok: false };
+        }
+    }, []);
+
+    return { getCurrentTab };
+};
