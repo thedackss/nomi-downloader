91f2fd649fbbdfa6dfa5716c96a1868afdbc5858
Diego Zamora <diegozar02@gmail.com>
2026-02-19 20:18:04 +0000

    Implement getMedias and downloadAlbum methods for media handling and downloading

--- src/background/nomi/index.ts
+++ src/background/nomi/index.ts
@@ -7,6 +7,13 @@ import type {
     Message,
     SelfieRequest,
 } from "../../interfaces/nomi/api.nomis.id.chat";
+import type {
+    APINomisIDMediasResponse,
+    Media,
+} from "../../interfaces/nomi/api.nomis.id.medias";
+import type { GetMediasProps } from "./interfaces/getMedias";
+import JSZip from "jszip";
+import type { DownloadAlbumProps } from "./interfaces/downloadAlbum";
 
 interface NomiErrorProps {
     id?: number;
@@ -109,24 +116,309 @@ export class Nomi {
         }
     }
 
-    public async downloadAlbum(
-        nomiId: string,
-        updateStatus: (message: string) => void,
-    ) {
-        Log("Downloading album for Nomi ID: " + nomiId);
-        updateStatus("Initializing download...");
+    public async getMedias({ nomiId, onProgress }: GetMediasProps) {
+        Log("Getting media for Nomi ID: " + nomiId);
 
-        await new Promise((resolve) => setTimeout(resolve, 1000));
-        updateStatus("Fetching images...");
+        const exists = await this.exists({ nomiId });
 
-        for (let i = 0; i < 100; i++) {
-            updateStatus(`Processing image ${i + 1} of 100`);
-            await new Promise((resolve) => setTimeout(resolve, 750));
+        const mediasUrl = `/nomis/${nomiId}/medias`;
+
+        if (exists) {
+            const { data } = await api.get<APINomisIDMediasResponse>(mediasUrl);
+
+            const totalPages = data.maxPages;
+            const selfies: Media[] = [];
+
+            for (let i = 1; i <= totalPages; i++) {
+                const url = `${mediasUrl}?page=${i}`;
+
+                const { data } = await api.get<APINomisIDMediasResponse>(url);
+
+                const newSelfies = data.medias;
+                const totalFoundSoFar = selfies.length + newSelfies.length;
+
+                // Determine increment step based on total count
+                let step = 1;
+                if (totalFoundSoFar > 500) step = 50;
+                else if (totalFoundSoFar > 300) step = 20;
+                else if (totalFoundSoFar > 100) step = 10;
+
+                // Simulate smooth counting
+                let currentCount = selfies.length;
+                while (currentCount < totalFoundSoFar) {
+                    const remaining = totalFoundSoFar - currentCount;
+
+                    // Force step to 1 for the last 10 items
+                    let currentStep = step;
+                    let currentMs = 10;
+
+                    if (i === totalPages) currentMs = 100;
+                    if (remaining <= 50) currentStep = 5;
+                    if (remaining <= 20) currentStep = 2;
+                    if (remaining <= 10) currentStep = 1;
+
+                    currentCount += currentStep;
+
+                    if (currentCount > totalFoundSoFar)
+                        currentCount = totalFoundSoFar;
+
+                    const log = `[Scanning]: ${currentCount} selfies found...`;
+                    if (onProgress) onProgress(log);
+
+                    // Tiny delay to make it visible but not slow
+                    await new Promise((resolve) =>
+                        setTimeout(resolve, currentMs),
+                    );
+                }
+
+                selfies.push(...newSelfies);
+            }
+
+            return selfies.sort((a, b) => {
+                const dateA = new Date(a.completed);
+                const dateB = new Date(b.completed);
+
+                return dateA.getTime() - dateB.getTime();
+            });
+        } else {
+            throw new NomiError({
+                id: nomiId,
+                message: "Nomi with ID " + nomiId + " does not exist",
+            });
         }
+    }
 
-        await new Promise((resolve) => setTimeout(resolve, 1000));
-        updateStatus("Zipping files...");
+    public async downloadAlbum({
+        nomiId,
+        onProgress,
+        quality = "HD",
+        folderization = false,
+        downloadQuantity = 10,
+    }: DownloadAlbumProps) {
+        try {
+            Log("Downloading album for Nomi ID: " + nomiId);
+            const qualityBasedExtension = quality === "HD" ? "png" : "webp";
 
-        Log("Album downloaded!");
+            function update(message: string) {
+                if (onProgress) onProgress(message);
+            }
+
+            update("Checking if Nomi exists...");
+
+            const medias = await this.getMedias({
+                nomiId,
+                onProgress,
+            });
+
+            if (medias.length === 0) {
+                throw new NomiError({
+                    id: nomiId,
+                    message: "No media found for Nomi with ID " + nomiId,
+                });
+            }
+
+            const estimateMediaSize = (media: Media) => {
+                let size = 0;
+
+                // HD Photo ~2MB
+                if (media.type === "Photo" || media.type === "Art")
+                    if (quality === "HD") size = 2 * 1024 * 1024;
+                    else size = 0.1 * 1024 * 1024;
+                // PhotoEdit ~2MB
+                else if (media.mediaType === "ImageEditRequest")
+                    if (quality === "HD") size = 2 * 1024 * 1024;
+                    else size = 0.1 * 1024 * 1024;
+                // Video ~5MB
+                else if (media.mediaType === "VideoRequest")
+                    size = 5 * 1024 * 1024;
+                // Default ~1MB
+                else size = 1 * 1024 * 1024;
+
+                return size;
+            };
+
+            function chunkMedias(
+                medias: Media[],
+                maxBytesPerChunk = 750 * 1024 * 1024,
+            ) {
+                const chunks: Media[][] = [];
+                let currentChunk: Media[] = [];
+                let currentSize = 0;
+
+                for (const media of medias) {
+                    const size = estimateMediaSize(media);
+
+                    if (
+                        currentSize + size > maxBytesPerChunk &&
+                        currentChunk.length > 0
+                    ) {
+                        chunks.push(currentChunk);
+                        currentChunk = [];
+                        currentSize = 0;
+                    }
+
+                    currentChunk.push(media);
+                    currentSize += size;
+                }
+
+                if (currentChunk.length > 0) {
+                    chunks.push(currentChunk);
+                }
+
+                return chunks;
+            }
+
+            const chunks = chunkMedias(medias);
+            const downloads: { url: string; filename: string }[] = [];
+
+            for (let chunkIndex = 0; chunkIndex < chunks.length; chunkIndex++) {
+                const chunk = chunks[chunkIndex];
+                const zip = new JSZip();
+
+                let chunkMesage = `Chunk ${chunkIndex + 1}/${chunks.length}, `;
+                if (chunks.length < 2) chunkMesage = "";
+
+                update(
+                    `[Processing]: ${chunkMesage}${chunk.length} media items`,
+                );
+
+                const processMedia = async (media: Media, i: number) => {
+                    type MediaType = "Photo" | "Video" | "Art" | "PhotoEdit";
+
+                    let type: MediaType = "Photo";
+                    let isMp4 = false;
+                    let url = "";
+
+                    // Determine media type
+                    if (media.type) {
+                        if (media.type === "Art") type = "Art";
+                        else if (media.type === "Photo") type = "Photo";
+                    }
+
+                    if (media.mediaType === "VideoRequest") type = "Video";
+                    else if (media.mediaType === "ImageEditRequest")
+                        type = "PhotoEdit";
+
+                    // Determine URL based on type
+                    if (type === "Photo" || type === "Art") {
+                        url = `/selfie-requests/${media.selfieRequestId}/images/${media.id}.${qualityBasedExtension}`;
+                    } else if (type === "Video") {
+                        const mp4Url = `/video-requests/${media.uuid}.mp4`;
+                        const webpUrl = `/video-requests/${media.uuid}.webp`;
+                        try {
+                            await api.head(mp4Url);
+                            url = mp4Url;
+                            isMp4 = true;
+                        } catch {
+                            url = webpUrl;
+                        }
+                    } else if (type === "PhotoEdit") {
+                        url = `/image-edit-requests/${media.uuid}/edited-image.${qualityBasedExtension}`;
+                    }
+
+                    let blob: Blob;
+
+                    const stringDate = new Date(media.completed)
+                        .toISOString()
+                        .split("T")[0];
+
+                    const chunkOffset = chunks
+                        .slice(0, chunkIndex)
+                        .reduce((acc, c) => acc + c.length, 0);
+
+                    // Ensure index is unique across chunks by adding offset
+                    const globalIndex = i + chunkOffset;
+
+                    function getPath(mediaType: MediaType) {
+                        let ext: string = qualityBasedExtension;
+
+                        if (mediaType === "Video") {
+                            if (isMp4) ext = "mp4";
+                            else ext = "webp";
+                        }
+
+                        // If Nomi.name is static or inherited, access it properly.
+                        const baseName = `nomi_${nomiId}`;
+                        const folder = mediaType.toLowerCase();
+                        const path = `${baseName}_${globalIndex}_${stringDate}.${ext}`;
+
+                        if (folderization) return `${folder}/${path}`;
+                        else return path;
+                    }
+
+                    update(
+                        `[Processing]: ${chunkMesage}Downloading media ${globalIndex + 1}/${medias.length}`,
+                    );
+
+                    try {
+                        // Increase timeout for large HD files
+                        const { data } = await api.get(url, {
+                            responseType: "blob",
+                            timeout: 60000, // 60 seconds timeout
+                        });
+
+                        blob = data;
+
+                        zip.file(getPath(type), blob);
+                    } catch (error) {
+                        update(
+                            `[Processing]: ${chunkMesage}Error downloading media ${globalIndex + 1}/${medias.length}, skipping`,
+                        );
+
+                        Log(
+                            `Failed to download media ID ${media.id} from URL ${url}:`,
+                            error,
+                        );
+                    }
+                };
+
+                for (let i = 0; i < chunk.length; i += downloadQuantity) {
+                    const promises = [];
+                    for (let j = 0; j < downloadQuantity; j++) {
+                        const index = i + j;
+                        if (index < chunk.length) {
+                            promises.push(processMedia(chunk[index], index));
+                        }
+                    }
+
+                    await Promise.all(promises);
+                }
+
+                const zipFile = await zip.generateAsync({ type: `base64` });
+                const dataUrl = "data:application/zip;base64," + zipFile;
+
+                const date = new Date().toISOString().replace(/:/g, "-");
+                const filename = `nomi-album-${nomiId}-${date}-part${chunkIndex + 1}.zip`;
+
+                downloads.push({ url: dataUrl, filename });
+
+                console.log(
+                    `Chunk ${chunkIndex + 1} processed. Found ${medias.length} items.`,
+                );
+            }
+
+            for (const download of downloads) {
+                try {
+                    await chrome.downloads.download({
+                        url: download.url,
+                        filename: download.filename,
+                        saveAs: false,
+                    });
+                } catch (err) {
+                    Log("Download failed", err);
+                }
+            }
+
+            await new Promise((resolve) => setTimeout(resolve, 1000));
+        } catch (error) {
+            if (error instanceof NomiError) {
+                Log("NomiError: " + error.message);
+                if (onProgress) onProgress("Error: " + error.message);
+            } else {
+                Log("Unexpected error during album download:", error);
+                if (onProgress)
+                    onProgress("An unexpected error occurred during download.");
+            }
+        }
     }
 }
