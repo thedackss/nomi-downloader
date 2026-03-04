3ff260cc37dbec9cecc955a1eed55a38112810f7
Diego Zamora <diegozar02@gmail.com>
2026-03-04 11:20:03 +0000

    refactor: overhaul Nomi list UI components and update Nomi/GroupChat API interfaces.

--- src/interfaces/nomi/api.groupChats.ts
+++ src/interfaces/nomi/api.groupChats.ts
@@ -2,114 +2,170 @@ export interface ApiGroupChatsResponse {
     groupChats: GroupChat[];
 }
 
-interface GroupChat {
-    id: number;
-    userId: number;
-    name: string;
-    status: string;
-    nextSpeakerNomiId: any;
-    deleted: any;
-    updated: string;
-    created: string;
-    backchannelingEnabled: boolean;
-    artSettings: ArtSettings;
-    settings: Settings;
-    type: string;
-    uuid: string;
-    generatingSpeechGroupChatMessageId: any;
-    mostRecentMessage: MostRecentMessage;
-    note: Note;
-    messageDraft: string;
-    hasActiveArtRequest: boolean;
-    nomis: Nomi[];
-    socketToken: string;
+export interface GroupChat {
+    id:                                 number;
+    userId:                             number;
+    name:                               string;
+    status:                             Status;
+    nextSpeakerNomiId:                  null;
+    deleted:                            null;
+    updated:                            Date;
+    created:                            Date;
+    backchannelingEnabled:              boolean;
+    artSettings:                        GroupChatArtSettings;
+    settings:                           GroupChatSettings;
+    type:                               string;
+    uuid:                               string;
+    generatingSpeechGroupChatMessageId: null;
+    mostRecentMessage:                  MostRecentMessage;
+    note:                               Note;
+    messageDraft:                       string;
+    hasActiveArtRequest:                boolean;
+    nomis:                              Nomi[];
+    socketToken:                        string;
 }
 
-interface ArtSettings {
-    aspectRatio: any;
-    imageStyle: string;
+export interface GroupChatArtSettings {
+    aspectRatio: null;
+    imageStyle:  ImageStyle;
 }
 
-interface Settings {
-    imageSystem: number;
-    imageStyle: string;
-    videoResolution: string;
+export enum ImageStyle {
+    Anime = "Anime",
+    Realistic = "Realistic",
 }
 
-interface MostRecentMessage {
+export interface MostRecentMessage {
     uuid: string;
     text: string;
-    sent: string;
+    sent: Date;
 }
 
-interface Note {
-    draft: string;
-    text: string;
-    status: string;
-    roleplay: string;
-    roleplayDraft: string;
-    roleplayStatus: string;
-}
-
-interface Nomi {
-    id: number;
-    userId: number;
-    name: string;
-    gender: string;
-    created: string;
-    updated: string;
-    relationshipType: string;
-    archived?: string;
-    status: string;
-    characterId: string;
-    pictureImageId: string;
-    pictureSelfieImageId?: string;
-    settings: Settings2;
-    artSettings: ArtSettings2;
-    customTraits: CustomTrait[];
-    keyTraits: any[];
-    generatingSpeechChatMessageId: any;
-    communicationStyle: string;
-    useAvvi: boolean;
-    elevenLabsVoiceId: any;
-    hasPendingAutoSelfiesRequest: boolean;
-    uuid: string;
+export interface Nomi {
+    id:                               number;
+    userId:                           number;
+    name:                             string;
+    gender:                           Gender;
+    created:                          Date;
+    updated:                          Date;
+    relationshipType:                 RelationshipType;
+    archived:                         Date | null;
+    status:                           Status;
+    characterId:                      string;
+    pictureImageId:                   string;
+    pictureSelfieImageId:             null | string;
+    settings:                         NomiSettings;
+    artSettings:                      NomiArtSettings;
+    customTraits:                     CustomTrait[];
+    keyTraits:                        any[];
+    generatingSpeechChatMessageId:    null;
+    communicationStyle:               Status;
+    useAvvi:                          boolean;
+    elevenLabsVoiceId:                null;
+    hasPendingAutoSelfiesRequest:     boolean;
+    uuid:                             string;
     hasEverEnabledProactiveMessaging: boolean;
-    customVoiceId: any;
-    voiceSource: string;
-    builtInVoiceId: any;
-    imageEditRequestUuid: any;
-    hasUnreadMedia: boolean;
-    optOutRoleplayStarter?: boolean;
-    videoRequestUuid?: string;
-    baseCharacterImageId?: string;
-    baseSelfieImageId?: string;
-    removed: boolean;
+    customVoiceId:                    null;
+    voiceSource:                      VoiceSource;
+    builtInVoiceId:                   null;
+    imageEditRequestUuid:             null;
+    hasUnreadMedia:                   boolean;
+    optOutRoleplayStarter:            boolean | null;
+    videoRequestUuid:                 null | string;
+    baseCharacterImageId:             null | string;
+    baseSelfieImageId:                null | string;
+    baseImageEditRequestUuid:         null;
+    removed:                          boolean;
 }
 
-interface Settings2 {
-    imageSystem?: number;
-    imageFilterSensitivity: string;
-    imageStyle: string;
-    selfiePhotoSystem: string;
-    selfieFaceFidelityRatio: number;
-    allowCoupleSelfies: boolean;
-    useBetaAi: boolean;
-    proactiveNomiMessageFrequency?: string;
-    allowAutomaticSelfies: boolean;
-    aiSystem: string;
-    videoResolution: string;
+export interface NomiArtSettings {
+    fidelityWeight:     number;
+    faceMatch:          boolean;
+    aspectRatio:        AspectRatio;
+    style:              Style;
+    poseReference:      null | string;
+    nomiAnchorLookUuid: null;
 }
 
-interface ArtSettings2 {
-    fidelityWeight: number;
-    faceMatch: boolean;
-    aspectRatio: string;
-    style: string;
-    poseReference?: string;
+export enum AspectRatio {
+    Horizontal = "horizontal",
+    Square = "square",
+    Vertical = "vertical",
 }
 
-interface CustomTrait {
+export enum Style {
+    Artistic = "Artistic",
+    Realistic = "Realistic",
+}
+
+export enum Status {
+    Default = "Default",
+    Descriptive = "Descriptive",
+}
+
+export interface CustomTrait {
     name: string;
     type: string;
+}
+
+export enum Gender {
+    Female = "Female",
+    Male = "Male",
+}
+
+export enum RelationshipType {
+    Friend = "Friend",
+    Roleplay = "Roleplay",
+}
+
+export interface NomiSettings {
+    imageSystem:                   number | null;
+    imageFilterSensitivity:        ImageFilterSensitivity;
+    imageStyle:                    ImageStyle;
+    selfiePhotoSystem:             SelfiePhotoSystem;
+    selfieFaceFidelityRatio:       number;
+    allowCoupleSelfies:            boolean;
+    useBetaAi:                     boolean;
+    proactiveNomiMessageFrequency: null | string;
+    allowAutomaticSelfies:         boolean;
+    aiSystem:                      AISystem;
+    videoResolution:               VideoResolution;
+    nomiAnchorLookUuid:            null;
+}
+
+export enum AISystem {
+    Solstice = "Solstice",
+}
+
+export enum ImageFilterSensitivity {
+    Moderate = "Moderate",
+}
+
+export enum SelfiePhotoSystem {
+    PhotosV3 = "photos-v3",
+    PhotosV4 = "photos-v4",
+}
+
+export enum VideoResolution {
+    HD = "hd",
+    SD = "sd",
+}
+
+export enum VoiceSource {
+    BuiltIn = "BuiltIn",
+}
+
+export interface Note {
+    draft:          string;
+    text:           string;
+    status:         Status;
+    roleplay:       string;
+    roleplayDraft:  string;
+    roleplayStatus: Status;
+}
+
+export interface GroupChatSettings {
+    imageSystem:     number;
+    imageStyle:      ImageStyle;
+    videoResolution: VideoResolution;
 }
