32fa2e9241f90a769255b4033680bcb4f809b0bd
Diego Zamora <diegozar02@gmail.com>
2026-03-19 22:30:54 +0000

    refactor: Simplify background image handling in Info component for mobile view

--- src/popup/components/Info/index.tsx
+++ src/popup/components/Info/index.tsx
@@ -36,23 +36,26 @@ export const Info = () => {
     const Nomi = Nomis.selected.nomi;
     const Group = Nomis.selected.group;
 
-    const bgImages = isMobile
-        ? Nomi
-            ? [getNomiImageUrl(Nomi)]
-            : Group
-              ? Group.nomis.slice(0, 4).map(getNomiImageUrl)
-              : []
-        : [];
+    const bgImages: string[] = [];
 
-    const bgStyle =
-        bgImages.length > 0
-            ? ({
-                  "--bg-image-1": `url(${bgImages[0]})`,
-                  ...(bgImages[1] && { "--bg-image-2": `url(${bgImages[1]})` }),
-                  ...(bgImages[2] && { "--bg-image-3": `url(${bgImages[2]})` }),
-                  ...(bgImages[3] && { "--bg-image-4": `url(${bgImages[3]})` }),
-              } as React.CSSProperties)
-            : undefined;
+    if (isMobile) {
+        if (Nomi) {
+            bgImages.push(getNomiImageUrl(Nomi));
+        } else if (Group) {
+            bgImages.push(...Group.nomis.slice(0, 4).map(getNomiImageUrl));
+        }
+    }
+
+    let bgStyle: React.CSSProperties | undefined;
+    if (bgImages.length > 0) {
+        bgStyle = {
+            "--bg-image-1": `url(${bgImages[0]})`,
+            ...(bgImages[1] && { "--bg-image-2": `url(${bgImages[1]})` }),
+            ...(bgImages[2] && { "--bg-image-3": `url(${bgImages[2]})` }),
+            ...(bgImages[3] && { "--bg-image-4": `url(${bgImages[3]})` }),
+        } as React.CSSProperties;
+    }
+
     useEffect(() => {
         async function main() {
             setLoading(false);
